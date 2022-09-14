c864fe75c966abdbe35d771037d770343ff9cb8c
Yassin Zairi <74818188+Yzairi@users.noreply.github.com>
2022-09-14 23:57:38 +0200

    add équipe page

---
+++ projet/equipes.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bordeaux-basket</title>
+    <link rel="stylesheet" href="css/equipe.css">
+    <!-- icons  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="icon" type="img/png" href="images/logo.png">
+</head>
+<body>
+    <!-- header  -->
+    <?php include("header.php"); ?>
+        <div class="container equipes">
+            <div class="equipe1 opp">
+                <div class="text">
+                    <h4>Seniors Masculins 1</h4>
+                    <ul>
+                        <li>BAILLY Charles</li>
+                        <li>BLANC Maxime</li>
+                        <li>BOUSSEKHANE Hamid</li>
+                        <li>HAMLAOUI Sofiane</li>
+                        <li>LALANNE Thomas</li>
+                        <li>LARREY Théo</li>
+                        <li>LEFEBVRE Ulysse</li>
+                        <li>LONGOK William</li>
+                        <li>MAHOUTO Wilfried</li>
+                        <li>MERGERIE Jocelin</li>
+                    </ul>
+                </div>
+                <div class="image">
+                <img src="images/equipe1.png" alt="">
+                </div>
+            </div>
+            <div class="equipe2 opp">
+                <div class="text">
+                    <h4>Seniors Féminines</h4>
+                    <ul>
+                        <li>BRUNA Clrence</li>
+                        <li>DEGRYSE May loou</li>
+                        <li>HIGOUNENC Gabrielle</li>
+                        <li>PACREAU Lou</li>
+                        <li>QUINIO Mia</li>
+                        <li>RITAL Yasmine</li>
+                        <li>TOULY Alexane</li>
+                        <li>TSANY Kimberlay</li>
+                        <li>URRUTIA Mathilde</li>
+                        <li>YACIA Myriam</li>
+                    </ul>
+                </div>
+                <div class="image">
+                <img src="images/equipe2.png" alt="">
+                </div>
+            </div>
+            <div class="equipe3 opp">
+                <div class="text">
+                    <h4>U13 M2</h4>
+                    <ul>
+                        <li>AIT TALEB Ruben</li>
+                        <li>ALAOUI Ilys</li>
+                        <li>ARQUE Anoé</li>
+                        <li>BARNABE Ella</li>
+                        <li>BONAT RIGOLLET Milo</li>
+                        <li>CARALP-PRIVAT Bérénice</li>
+                        <li>CHOULAK Yonnes</li>
+                        <li>DO Marley</li>
+                        <li>EYMERI Diego</li>
+                        <li>FUMADELLES Sacha</li>
+                        <li>JOUANNEAU Martin</li>
+                        <li>RICAUD Louise</li>
+                    </ul>
+                </div>
+                <div class="image">
+                <img src="images/equipe3.png" alt="">
+                </div>
+            </div>
+        </div>
+    <!-- footer -->
+    <?php include("footer.php"); ?>
+</body>
+</html>
