1d4801fab08115769c990a7e2b54d73288d45bb9
Yassin Zairi <74818188+Yzairi@users.noreply.github.com>
2022-09-14 23:13:02 +0200

    commit boutique page

---
+++ projet/boutique.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bordeaux-basket</title>
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- icons  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="icon" type="img/png" href="images/logo.png">
+</head>
+<body>
+    <!-- header  -->
+    <?php include("header.php"); ?>
+    <main>
+       <h4  >coming soon !</h4>
+       <br>
+       <style>
+        h4 {
+            font-size:2em;
+            text-align: center;
+            padding: 50px;
+            margin:113px 10px;
+            color:#2f435e;
+            text-transform: uppercase;
+        }
+       </style>
+    </main>
+    <!-- footer -->
+    <?php include("footer.php"); ?>
+</body>
+</html>
